6615bc9ea5e7bb49292f398a4040c83e5b2ef9d2
student <student@EDAs-MacBook-Pro.local>
2025-02-25 15:22:35 -0600

    post endpoint created and only admin can craete design

--- server/routes/design.router.js
+++ server/routes/design.router.js
@@ -5,7 +5,32 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 
 const router = express.Router();
 
-
+// post designs only for admin
+router.post('/', rejectUnauthenticated, async (req, res) => {
+  
+    const { title, image_url, price, description } = req.body;
+  
+    const query = `
+      INSERT INTO designs (title, image_url, price, description) 
+      VALUES ($1, $2, $3, $4) RETURNING *`;
+  
+    try {
+      const result = await pool.query(query, [title, image_url, price, description]);
+  
+      // send the message and design 
+      // res.status(201).send(result.rows[0]);  // Send back the newly created design it
+      // same thing as below code except retaining message and name design
+  
+      res.status(201).send({
+        message: 'Design created successfully',
+        design: result.rows[0] 
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ error: 'Error creating design' });
+    }
+  });
+  
 // designs this for public
 router.get('/', async (req, res) => {
   const query = 'SELECT * FROM designs';
